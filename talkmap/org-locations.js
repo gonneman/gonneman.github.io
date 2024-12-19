575863b96c78a6bca25b0ce1afda213b89a7a951
Simon Wood <simon.james.wood@gmail.com>
2024-12-19 16:18:15 +0900

    Updated talk locations.

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,37 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Canberra, Australia",
+    -35.2975906,
+    149.1012676
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Taipei, Taiwan",
+    25.0375198,
+    121.5636796
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Bonn, Germany",
+    50.7352621,
+    7.1024635
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Tokyo, Japan",
+    35.6768601,
+    139.7638947
+  ],
+  [
+    "Stony Brook, New York, USA",
+    40.9256538,
+    -73.140943
+  ],
+  [
+    "Albany, New York, USA",
+    42.6511674,
+    -73.754968
+  ],
+  [
+    "Edmonton, Canada",
+    53.5462055,
+    -113.491241
   ]
 ];
